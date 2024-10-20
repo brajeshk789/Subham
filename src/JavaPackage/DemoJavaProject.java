5368130ead632a44a2ac0ea48fabf4d9a443ad59
brajeshk789 <brajeshk789@gmail.com>
2024-10-20 11:34:26 +0530

    First commit

---
+++ src/JavaPackage/DemoJavaProject.java
@@ -0,0 +1,13 @@
+package JavaPackage;
+
+public class DemoJavaProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.setProperty("webdriver.chrome.driver","path\\chromedriver.exe");
+	
+	
+	WebDriver driver = new ChromeDriver();
+			}
+
+}
